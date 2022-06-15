e2381dfcf6f6431e164a81384667cd0b4305bfc2
Akifffff <abdakif12@gmail.com>
2022-06-15 23:18:28 +0800

    commit connect database 7

--- configure_database.php
+++ configure_database.php
@@ -3,14 +3,14 @@
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'csc443 project');
+define('DB_NAME', 'projectjkr');
  
 
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-$result = mysqli_query($link, "select * from client");
 
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
 
-mysqli_free_result($result);
-mysqli_close($link);
 ?>
 
