c620464cdd8912db224c54fe79b8b0410541adc0
Akifffff <abdakif12@gmail.com>
2022-06-15 22:59:46 +0800

    commit connect db 5

--- configure_database.php
+++ configure_database.php
@@ -7,8 +7,13 @@ define('DB_NAME', 'csc443 project');
  
 
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-
-if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
+$result = mysqli_query($link, "select * from client");
+while ($row = mysqli_fetch_array ($result)){
+    foreach($row as $key =>$value){
+        echo "$key = $value <br> \n";
+    }
 }
+mysqli_free_result($result);
+mysqli_close($link);
 ?>
+
